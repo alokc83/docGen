7d0e89169f3f354dae20092c061a38a1eeede7c5
Alok Choudhary <ac687j@att.com>
2016-06-10 23:00:24 -0400

    Added comment
    comment

--- Swift/swiftDocumentation.sh
+++ Swift/swiftDocumentation.sh
@@ -3,3 +3,5 @@ SWIFT_VERSION="2.1.1"
 COPYRIGHT="Mt. Aden 2016"
 jazzy --swift-version $SWIFT_VERSION --clean --min-acl internal --author "Alok Choudhary" --xcodebuild-arguments -scheme,FaceMorpheus --output docs --docset-path docset --copyright $COPYRIGHT \
     --skip-undocumented
+    #comment
+    
