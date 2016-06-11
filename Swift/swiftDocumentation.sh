1e6797b9fac4f1c091cf0fc106d4fec62dc452fe
Alok Choudhary <ac687j@att.com>
2016-06-10 23:40:27 -0400

    added swift-version read from cmdline

--- Swift/swiftDocumentation.sh
+++ Swift/swiftDocumentation.sh
@@ -1,7 +1,13 @@
 #jazzy documentation
+
+#vars
 SWIFT_VERSION="2.1.1"
 COPYRIGHT="Mt. Aden 2016"
-jazzy --swift-version $SWIFT_VERSION --clean --min-acl internal --author "Alok Choudhary" --xcodebuild-arguments -scheme,FaceMorpheus --output docs --docset-path docset --copyright $COPYRIGHT \
+
+swift-ver = `swift --version | awk -v OFS=' ' '{print $4}'`
+echo $swift-ver
+#ducumentation
+
+jazzy --swift-version $swift-ver --clean --min-acl internal --author "Alok Choudhary" --xcodebuild-arguments -scheme,FaceMorpheus --output docs --docset-path docset --copyright $COPYRIGHT \
     --skip-undocumented
     #comment
-    
